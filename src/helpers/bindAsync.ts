899ab40b63139a119d950546cb3292484438f72e
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-23 13:32:27 +0200

    feat: added a bindAsync helper

--- src/helpers/bindAsync.ts
+++ src/helpers/bindAsync.ts
@@ -2,7 +2,7 @@ import { Mapper } from '../internalTypes'
 import { Option, None } from '../types'
 import { match } from './match'
 
-export const bind = <T, U>(
+export const bindAsync = <T, U>(
     binder: Mapper<T, Promise<Option<U>>>,
     option: Option<T>
 ): Promise<Option<U>> => {
